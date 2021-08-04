c1f5b7ad5de7ef5539b38bdcd5f1cb116864f6b1
Ibrahimsam96 <Ibrahimsam96@Gmail.com>
2021-08-04 00:56:10 -0400

    Index.js/GlobalCss

---
+++ components/navigationBar.js
@@ -0,0 +1,24 @@
+const NavigationBar  = ( ) => {
+
+return (
+
+<div className="NavBar">
+
+  <a className="Nav1" href={"/"}>
+  <span>
+   {`< `} 
+  </span>
+
+<span >
+Ibrahim Samara
+</span>
+
+    <span>
+   {` />`} 
+  </span>
+  </a>
+</div>
+    )
+}
+
+export default NavigationBar;
